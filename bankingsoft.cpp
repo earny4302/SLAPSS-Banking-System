056af0c665616b53ec1b5e8653ff163644b0a9cd
earny4302 <73746161+earny4302@users.noreply.github.com>
2021-02-13 18:17:01 +0530

    Update bankingsoft.cpp

--- bankingsoft.cpp
+++ bankingsoft.cpp
@@ -4,6 +4,40 @@
 using namespace std;
 int main()
 {
+    int choice;
+    std::cin<<choice;
+    switch(choice)
+    {
+        case 1:
+            break;
+            
+        case 2:
+            break;
+            
+        case 3:
+            break;
+        
+        case 4:
+            break;
+        
+        case 5:
+            break;
+        
+        case 6:
+            break;
+        
+        case 7:
+            break;
+        
+        case 8:
+            break;
+        
+        case 9:
+            break;
+        
+        default:
+
+    }
     
     return 0;
 }
