f3ffb100bb1bf5c10164b3f40629af771dc0be32
earny4302 <73746161+earny4302@users.noreply.github.com>
2021-02-14 16:50:00 +0530

    Update bankingsoft.cpp

--- bankingsoft.cpp
+++ bankingsoft.cpp
@@ -1,70 +1,70 @@
-#include<iostream>
-#include<conio.h>
+#include <iostream>
+#include <conio.h>
 
 using namespace std;
 int main()
-{   frontscreen:
-    std::cout<<"\t\t\t\t\t\t\t\t\t\tWELCOME TO SLAPPS BANK"<<endl;
-    std::cout<<"PLEASE SELECT FROM GIVEN"<<endl;
-    std::cout<<""<<endl;
-    std::cout<<""<<endl;
-    std::cout<<""<<endl;
-    std::cout<<""<<endl;
-    std::cout<<""<<endl;
-    std::cout<<""<<endl;
-    std::cout<<""<<endl;
-    std::cout<<""<<endl;
-    std::cout<<""<<endl;
-    std::cout<<""<<endl;
-    std::cout<<""<<endl;
-    std::cout<<""<<endl;
-    std::cout<<""<<endl;
-    std::cout<<""<<endl;
-    std::cout<<""<<endl;
-    std::cout<<""<<endl;
-    std::cout<<""<<endl;
-   
-
-    
+{
     int choice;
-    std::cout<<"ENTER YOUR CHOICE(1-9):-"<<endl;
-    std::cin>>choice;
-    switch(choice)
+
+    do
     {
+        std::cout << "\t\t\t\t\t\t\t\t\t\tWELCOME TO SLAPPS BANK" << endl;
+        std::cout << "MAIN MENU:-" << endl;
+        std::cout << "01.GET PREPAID CARD" << endl;
+        std::cout << "" << endl;
+        std::cout << "" << endl;
+        std::cout << "" << endl;
+        std::cout << "" << endl;
+        std::cout << "" << endl;
+        std::cout << "" << endl;
+        std::cout << "" << endl;
+        std::cout << "" << endl;
+        std::cout << "" << endl;
+        std::cout << "" << endl;
+        std::cout << "" << endl;
+        std::cout << "" << endl;
+        std::cout << "" << endl;
+        std::cout << "" << endl;
+        std::cout << "" << endl;
+        std::cout << "" << endl;
+
+        std::cout << "ENTER YOUR CHOICE(1-9):-" << endl;
+        std::cin >> choice;
+        switch (choice)
+        {
         case 1:
-            
+
             break;
-            
+
         case 2:
             break;
-            
+
         case 3:
             break;
-        
+
         case 4:
             break;
-        
+
         case 5:
             break;
-        
+
         case 6:
             break;
-        
+
         case 7:
             break;
-        
+
         case 8:
             break;
-        
+
         case 9:
             break;
-        
-        default:
-            std::cout<<"WRONG CHOICE"<<endl;
-            getch();
-            goto frontscreen;
 
-    }
-    
+        default:
+            std::cout << "WRONG CHOICE" << endl;
+            getch();
+        }
+    } while (choice >= 9);
+
     return 0;
 }
