188da96db03f028ccec8332d9e861780451a38b8
earny4302 <73746161+earny4302@users.noreply.github.com>
2021-02-23 12:47:42 +0530

    Update bankingsoft.cpp

--- bankingsoft.cpp
+++ bankingsoft.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 #include <conio.h>
 #include <string>
-#include<stdlib.h>
+#include <stdlib.h>
 using namespace std;
 class account
 {
@@ -11,7 +11,7 @@ public:
     char father_n[60];
     char mother_n[60];
     char address[120];
-    char  nominee[60];
+    char nominee[60];
     long mob_no;
     long addhar_no;
     char login_user[10];
@@ -27,11 +27,17 @@ public:
     void pay();
     void electricity_pay();
     void water_pay();
+    void dth_pay();
+    void premobile_pay();
+    void fastag_pay();
+    void merchant_pay();
+    void book_train();
+    void book_bus();
 };
 
 void account::get_det()
 {
-    cout << "ENTER YOUR NAME:-" <<endl;
+    cout << "ENTER YOUR NAME:-" << endl;
     gets(my_name);
     cout << "ENTER YOUR FATHER'S NAME:-" << endl;
     gets(father_n);
@@ -50,89 +56,286 @@ void account::change_det()
     int choice;
     do
     {
-        cout<<"WHAT YOU WANT TO CHANGE:-"<<endl;
-        cout<<"01.NAME"<<endl;
-        cout<<"02.FATHER'S NAME"<<endl;
-        cout<<"03.MATHER'S NAME"<<endl;
-        cout<<"04.MOBILE NO."<<endl;
-        cout<<"05.ID NUMBER"<<endl;
-        cout<<"06.PIN NO"<<endl;
-        cout<<"ENTER YOUR CHOICE(1-6):-"<<endl;
-        cin>>choice;
+        cout << "WHAT YOU WANT TO CHANGE:-" << endl;
+        cout << "01.NAME" << endl;
+        cout << "02.FATHER'S NAME" << endl;
+        cout << "03.MATHER'S NAME" << endl;
+        cout << "04.MOBILE NO." << endl;
+        cout << "05.ID NUMBER" << endl;
+        cout << "06.PIN NO" << endl;
+        cout << "ENTER YOUR CHOICE(1-6):-" << endl;
+        cin >> choice;
 
-        switch(choice){
-            case 1:
-                cout<<"ENTER NAME:-"<<endl;
-                gets(my_name);
-                break;
-            case 2:
-                cout<<"ENTER FATHER'S NAME:-"<<endl;
-                gets(father_n);
-                break;
-            case 3:
-                cout<<"ENTER MOTHER'S NAME:-"<<endl;
-                gets(mother_n);
-                break;
-            case 4:
-                cout<<"ENTER MOBILE NUMBER:-"<<endl;
-                cin>>mob_no;
-                break;
-            case 5:
-                cout<<"ENTER ADDHAR NO.:-"<<endl;
-                cin>>addhar_no;
-                break;
-            case 6:
-                cout<<"ENTER PIN NO:-"<<endl;
-                cin>>pin_no;
-                break;
-            default:
-                cout<<"WRONG CHOICE"<<endl;
-                getch();
+        switch (choice)
+        {
+        case 1:
+            cout << "ENTER NAME:-" << endl;
+            gets(my_name);
+            break;
+        case 2:
+            cout << "ENTER FATHER'S NAME:-" << endl;
+            gets(father_n);
+            break;
+        case 3:
+            cout << "ENTER MOTHER'S NAME:-" << endl;
+            gets(mother_n);
+            break;
+        case 4:
+            cout << "ENTER MOBILE NUMBER:-" << endl;
+            cin >> mob_no;
+            break;
+        case 5:
+            cout << "ENTER ADDHAR NO.:-" << endl;
+            cin >> addhar_no;
+            break;
+        case 6:
+            cout << "ENTER PIN NO:-" << endl;
+            cin >> pin_no;
+            break;
+        default:
+            cout << "WRONG CHOICE" << endl;
+            getch();
         }
-    } while (choice>6);
+    } while (choice > 6);
 };
 void account::pay()
-{   int choice;
-    do{
-        cout<<"WHICH BILL YOU WANT TO PAY:-"<<endl;
-        cout<<"01.ELECTRICITY"<<endl;
-        cout<<"02.WATER"<<endl;
-        cout<<"03.DTH"<<endl;
-        cout<<"04.PREPAID MOBILLE"<<endl;
-        cout<<"05.FASTAG"<<endl;
-        cout<<"06.MEACHANT PAYMENT"<<endl;
-        cout<<"07.BOOK TRAIN"<<endl;
-        cout<<"08.BOOK BUS"<<endl;
+{
+    int choice;
+    do
+    {
+        cout << "WHICH BILL YOU WANT TO PAY:-" << endl;
+        cout << "01.ELECTRICITY" << endl;
+        cout << "02.WATER" << endl;
+        cout << "03.DTH" << endl;
+        cout << "04.PREPAID MOBILLE" << endl;
+        cout << "05.FASTAG" << endl;
+        cout << "06.MEACHANT PAYMENT" << endl;
+        cout << "07.BOOK TRAIN" << endl;
+        cout << "08.BOOK BUS" << endl;
 
-        switch(choice)
-        { 
-            case 1:
-                electricity_pay();
-                break;
-            case 2:
-                break;
-            case 3:
-                break;
-            case 4:
-                break;
-            case 5:
-                break;
-            case 6:
-                break;
-            case 7:
-                break;
-            case 8:
-                break;
-                 
+        switch (choice)
+        {
+        case 1:
+            electricity_pay();
+            break;
+        case 2:
+            water_pay();
+            break;
+        case 3:
+            dth_pay();
+            break;
+        case 4:
+            premobile_pay();
+            break;
+        case 5:
+            fastag_pay();
+            break;
+        case 6:
+            merchant_pay();
+            break;
+        case 7:
+            book_train();
+            break;
+        case 8:
+            book_bus();
+            break;
         }
 
-    }while(choice>9);
+    } while (choice > 9);
 };
+void account::electricity_pay()
+{
+    int amount;
+    long ivrs_no;
+    int pin1;
+    cout << "ENTER YOUR IVRS NO.:-\t";
+    cin >> ivrs_no;
+    cout << "\nENTER THE AMOUNT TO BE PAID:-\t";
+    cin >> amount;
+    cout << "\nENTER YOUR PIN NO:-\t";
+    cin >> pin1;
+    if (pin1 == pin_no)
+    {
+        if (acc_bal >= amount)
+        {
+            cout << "\nYOUR TRANSACTION IS SUCCESSFUL";
+            acc_bal = acc_bal - amount;
+            getch();
+        }
+        if (acc_bal < amount)
+        {
+            cout << "INSUFFIECIENT BALANCE";
+            getch();
+        }
+    }
+    if (pin1 != pin_no)
+    {
+        cout << "WRONG PIN";
+        getch();
+    }
+}
+void account::water_pay()
+{
+    int amount;
+    int pin1;
+    long id_no;
+    cout << "ENTER YOUR ID_NO:-\t";
+    cin >> id_no;
+    cout << "ENTER AMOUNT TO PAID:-\t";
+    cin >> amount;
+    cout << "ENTER PIN NO.:-\t";
+    cin >> pin1;
+    if (pin1 == pin_no)
+    {
+        if (acc_bal >= amount)
+        {
+            cout << "\nYOUR TRANSACTION IS SUCCESSFUL";
+            acc_bal = acc_bal - amount;
+            getch();
+        }
+        if (acc_bal < amount)
+        {
+            cout << "INSUFFIECIENT BALANCE";
+            getch();
+        }
+    }
+    if (pin1 != pin_no)
+    {
+        cout << "WRONG PIN";
+        getch();
+    }
+}
+void account::dth_pay()
+{
+    int amount;
+    long setupbox_no;
+    int pin1;
+    cout << "ENTER YOUR SETUP BOX NO:-\t";
+    cin >> setupbox_no;
+    cout << "ENTER AMOUNT TO PAID:-\t";
+    cin >> amount;
+    cout << "ENTER PIN NO.:-\t";
+    cin >> pin1;
+    if (pin1 == pin_no)
+    {
+        if (acc_bal >= amount)
+        {
+            cout << "\nYOUR TRANSACTION IS SUCCESSFUL";
+            acc_bal = acc_bal - amount;
+            getch();
+        }
+        if (acc_bal < amount)
+        {
+            cout << "INSUFFIECIENT BALANCE";
+            getch();
+        }
+    }
+    if (pin1 != pin_no)
+    {
+        cout << "WRONG PIN";
+        getch();
+    }
+}
+void account::premobile_pay()
+{  
+    int mobile_no;
+    int amount;
+    int pin1;
+
+    cout << "ENTER YOUR MOBILE NO:-\t";
+    cin >> mobile_no;
+    cout << "ENTER AMOUNT TO PAID:-\t";
+    cin >> amount;
+    cout << "ENTER PIN NO.:-\t";
+    cin >> pin1;
+    if (pin1 == pin_no)
+    {
+        if (acc_bal >= amount)
+        {
+            cout << "\nYOUR TRANSACTION IS SUCCESSFUL";
+            acc_bal = acc_bal - amount;
+            getch();
+        }
+        if (acc_bal < amount)
+        {
+            cout << "INSUFFIECIENT BALANCE";
+            getch();
+        }
+    }
+    if (pin1 != pin_no)
+    {
+        cout << "WRONG PIN";
+        getch();
+    }
+}
+void account::fastag_pay()
+{
+    int fastag_no;
+    int amount;
+    int pin1;
+
+    cout << "ENTER YOUR FASTAG CARD NO:-\t";
+    cin >> fastag_no;
+    cout << "ENTER AMOUNT TO PAID:-\t";
+    cin >> amount;
+    cout << "ENTER PIN NO.:-\t";
+    cin >> pin1;
+    if (pin1 == pin_no)
+    {
+        if (acc_bal >= amount)
+        {
+            cout << "\nYOUR TRANSACTION IS SUCCESSFUL";
+            acc_bal = acc_bal - amount;
+            getch();
+        }
+        if (acc_bal < amount)
+        {
+            cout << "INSUFFIECIENT BALANCE";
+            getch();
+        }
+    }
+    if (pin1 != pin_no)
+    {
+        cout << "WRONG PIN";
+        getch();
+    }
+}
+void account::merchant_pay(){
+    int shop_no;
+    int amount;
+    int pin1;
+
+    cout << "ENTER SHOP ID NO:-\t";
+    cin >> shop_no;
+    cout << "ENTER AMOUNT TO PAID:-\t";
+    cin >> amount;
+    cout << "ENTER PIN NO.:-\t";
+    cin >> pin1;
+    if (pin1 == pin_no)
+    {
+        if (acc_bal >= amount)
+        {
+            cout << "\nYOUR TRANSACTION IS SUCCESSFUL";
+            acc_bal = acc_bal - amount;
+            getch();
+        }
+        if (acc_bal < amount)
+        {
+            cout << "INSUFFIECIENT BALANCE";
+            getch();
+        }
+    }
+    if (pin1 != pin_no)
+    {
+        cout << "WRONG PIN";
+        getch();
+    }
+}
 
 int main()
-{   
+{
     account customer[100];
-    int opening_no=0;
+    int opening_no = 0;
     int current_no;
     int choice;
     do
@@ -142,7 +345,6 @@ int main()
         cout << "01.NEW COUSTOMER" << endl;
         cout << "02.LOGIN" << endl;
         cout << "03.EXIT" << endl;
-       
 
         cout << "ENTER YOUR CHOICE(1-3):-" << endl;
         cin >> choice;
@@ -153,18 +355,17 @@ int main()
             break;
 
         case 2:
-          
-            break;
-        
-        case 3: 
+
             break;
 
+        case 3:
+            break;
 
         default:
             cout << "WRONG CHOICE" << endl;
             getch();
         }
-    } while (choice >2);
+    } while (choice > 2);
 
     return 0;
 }
